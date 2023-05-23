170a1a90bc31ae2be5efc7a912eda1c60deb4069
babinhi <klyuio@naver.com>
2023-05-23 09:02:26 +0900

    장바구니 만들기 시작

---
+++ src/main/java/com/icia/shop/dto/CartDTO.java
@@ -0,0 +1,18 @@
+package com.icia.shop.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+public class CartDTO {
+    private int id;
+
+
+}
